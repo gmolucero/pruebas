63955d4b38de3171642f19b6fccc1da7ed095c2a
VÂíctor Droguett <victor@estudiosuma.cl>
2021-12-27 19:47:29 -0300

    se reestablecen componentes para edicion de profesion y renta del usuario

--- src/components/userComponent/UserEditAddressProfessionComponent.js
+++ src/components/userComponent/UserEditAddressProfessionComponent.js
@@ -6,25 +6,7 @@ import {
 } from "@coreui/react";
 import StepOneFormComponent from '../stepOneFormComponent/StepOneFormComponent';
 const UserEditAddressProfessionComponent = () => {
-    const [errorAge, setErrorAge] = React.useState(false);
-    const formik = useFormik({
-        initialValues: {
-            day: '',
-            month: '',
-            year: '',
-            country: '',
-            region: '',
-            commune: '',
-            education_level: '',
-            occupation: '',
-            other_occupation: ''
-        },
-        validate: validate(schema),
-        onSubmit: next,
-    });
-    return (<>
-        <StepOneFormComponent formik={formik} onChange={handleTextChange} errorAge={errorAge} />
-    </>);
+    return (<></>);
 };
 
 export default UserEditAddressProfessionComponent;
