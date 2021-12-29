47fae61d85e41cd911f51d70a6e74a82a3629e36
VÂíctor Droguett <victor@estudiosuma.cl>
2021-12-29 02:38:13 -0300

    componente profesión y direccion (pendiente guardar)

--- src/components/userComponent/UserEditAddressProfessionComponent.js
+++ src/components/userComponent/UserEditAddressProfessionComponent.js
@@ -1,12 +1,213 @@
-import React from "react";
+import React, { Fragment } from "react";
+// import PropTypes from 'prop-types';
+// import { updateCustomer, getCustomer } from 'services/customer';
+// import { getRegion, getComuneById } from 'services/location';
+// import { getEducationOptions, getProfession, getCountries } from 'services/lists';
+// import { useFormik } from "formik";
+// import { validate, handlerInputChangeCreator, isEmpty, getValidationResult } from 'utils';
+// import { stepOnechema as schema } from 'components/stepOneFormComponent/stepOnechema'
+import DateComponent from 'components/dateComponent/DateComponent';
+import Spinner from 'app/common/Spinner';
+import { getValidationResult } from 'utils';
+
 import {
-    CNav,
-    CNavItem,
-    CNavLink
+    CButton,
+    CForm,
+    CInput,
+    CSelect,
+    CInvalidFeedback,
+    CFormGroup,
+    CSpinner
 } from "@coreui/react";
-import StepOneFormComponent from '../stepOneFormComponent/StepOneFormComponent';
-const UserEditAddressProfessionComponent = () => {
-    return (<></>);
+
+const UserEditAddressProfessionComponent = ({ formik, countries, regions, communes, getCommunesByRegion, educations, professions }) => {
+    // let [timer, setTimer] = React.useState(null);
+    // const [countires, setCountries] = React.useState([]);
+    // const [region, setRegion] = React.useState([]);
+    // const [communes, setCommunes] = React.useState([]);
+    const [loadingCommunes, setLoadingCommunes] = React.useState(false);
+    // const [education, setEducation] = React.useState([]);
+    // const [profession, setProfession] = React.useState([]);
+    const [isMounted, setIsMounted] = React.useState(false);
+    // const [loading, setLoading] = React.useState(true);
+    const [errorAge, setErrorAge] = React.useState(false);
+    // const ERROR_MESSAGE = {
+    //     title: "",
+    //     text: "",
+    //     btnText: "Cerrar",
+    //     iconName: "cil-warning",
+    //     iconClassName: "text-danger",
+    //     btnOnClick: () => null,
+    // }
+    // const [modalConfig, setModalConfig] = React.useState({
+    //     show: false,
+    //     ...ERROR_MESSAGE
+    // })
+
+
+
+    // const handleInit = async () => {
+    //     try {
+
+    //     } catch (error) {
+    //     }
+    // }
+
+    function getAge(dateString) {
+        var today = new Date();
+        var birthDate = new Date(dateString);
+        var age = today.getFullYear() - birthDate.getFullYear();
+        var m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        if (age >= 18) {
+            return true;
+        } else {
+            return false;
+        }
+        // return age;
+    }
+
+    React.useEffect(() => {
+        if (formik.values.day && formik.values.month && formik.values.year) {
+            let valAge = getAge(formik.values.year + '/' + formik.values.month + '/' + formik.values.day)
+            if (valAge) {
+                setErrorAge(false);
+            } else {
+                setErrorAge(true);
+            }
+
+        }
+    }, [formik.values.day, formik.values.month, formik.values.year]);
+
+    React.useEffect(() => {
+        if (formik.values.region !== '') {
+            setLoadingCommunes((prevState) => !prevState);
+            if (isMounted) {
+                formik.setFieldValue('commune', '');
+            }
+            getCommunesByRegion(Number(formik.values.region));
+            setLoadingCommunes((prevState) => !prevState);
+        }
+    }, [formik.values.region]);
+
+    React.useEffect(() => {
+        setIsMounted(true);
+    }, [])
+
+    return (
+        <CForm className="my-5" onSubmit={formik.handleSubmit}>
+            <CFormGroup className="mb-3">
+                <label>¿Cuándo Naciste?</label>
+                <DateComponent custom size="small"
+                    day={Number(formik.values.day)}
+                    month={formik.values.month}
+                    year={formik.values.year}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur} />
+
+                <CInvalidFeedback
+                    className="d-block"
+                    invalid={getValidationResult(
+                        (formik.touched.day && !!formik.errors.day) ||
+                        (formik.touched.month && !!formik.errors.month) ||
+                        (formik.touched.year && !!formik.errors.year)
+                    )}>
+
+                    {[formik.errors.day, formik.errors.month, formik.errors.year].filter((el) => typeof el !== 'undefined').join(', ')}
+                </CInvalidFeedback>
+                {errorAge ? <p style={{ color: '#e55353' }}>Edad minima 18 años</p> : ''}
+
+            </CFormGroup>
+
+            <CFormGroup className="mb-3">
+                <CSelect custom name="country"
+                    value={formik.values.country}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}>
+                    <option disabled value="">Seleccione Nacionalidad ...</option>
+                    {
+                        countries.map((_country) => (<option key={_country.id} value={parseInt(_country.id)}>{_country.name}</option>))
+                    }
+                </CSelect>
+                <CInvalidFeedback className="d-inline" invalid={getValidationResult(!!formik.errors.country)}>{formik.errors.country}</CInvalidFeedback>
+            </CFormGroup>
+
+            <CFormGroup className="mb-3">
+                <CSelect custom name="region"
+                    value={formik.values.region}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}>
+                    <option disabled value="">Seleccione Region...</option>
+                    {
+                        regions.map((_region) => (<option key={_region.id} value={_region.id}>{_region.name}</option>))
+                    }
+                </CSelect>
+                <CInvalidFeedback className="d-inline" invalid={getValidationResult(!!formik.errors.region)}>{formik.errors.region}</CInvalidFeedback>
+            </CFormGroup>
+
+            <CFormGroup className="mb-3">
+                {(communes.length === 0 || loadingCommunes) ? <div className="text-center"><CSpinner color="light" /></div> :
+                    <CSelect custom name="commune"
+                        value={formik.values.commune}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}>
+                        <option disabled value="">Seleccione Comuna...</option>
+                        {
+                            communes.map((_commune) => (<option key={_commune.id} value={_commune.id}>{_commune.name}</option>))
+                        }
+                    </CSelect>}
+                <CInvalidFeedback className="d-inline" invalid={getValidationResult(!!formik.errors.commune)}>{formik.errors.commune}</CInvalidFeedback>
+
+            </CFormGroup>
+
+            <CFormGroup className="mb-3">
+                <CSelect custom name="education_level"
+                    value={formik.values.education_level}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}>
+                    <option disabled value="">Nivel de Estudios...</option>
+                    {
+                        educations.map((_education_level) => (<option key={_education_level.id} value={_education_level.nombre}>{_education_level.nombre}</option>))
+                    }
+                </CSelect>
+
+                <CInvalidFeedback className="d-inline" invalid={getValidationResult(!!formik.errors.education_level)}>{formik.errors.education_level}</CInvalidFeedback>
+            </CFormGroup>
+
+            <CFormGroup className="mb-3">
+                <CSelect custom name="occupation"
+                    value={formik.values.occupation}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}>
+                    <option disabled value="">¿A qué te dedicas?</option>
+                    {
+                        professions.map((_occupation) => (<option key={_occupation.id} value={_occupation.name}>{_occupation.name}</option>))
+                    }
+                </CSelect>
+                <CInvalidFeedback className="d-inline" invalid={getValidationResult(!!formik.errors.occupation)}>{formik.errors.occupation}</CInvalidFeedback>
+            </CFormGroup>
+
+            {
+                formik.values.occupation === 'Otro' &&
+                <CFormGroup className="mb-3">
+                    <CInput
+                        placeholder="A que te dedicas"
+                        value={formik.values.other_occupation}
+                        invalid={formik.touched.other_occupation && !!formik.errors.other_occupation}
+                        name="other_occupation"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
+                    <CInvalidFeedback invalid={getValidationResult(formik.touched.other_occupation && !!formik.errors.other_occupation)}>{formik.errors.other_occupation}</CInvalidFeedback>
+                </CFormGroup>
+            }
+            {!errorAge &&
+                <CButton type="submit" className="btn-login d-inline px-4" >Actualizar</CButton>
+            }
+        </CForm>
+    );
 };
 
 export default UserEditAddressProfessionComponent;
--- src/views/user/EditAddressProfession.js
+++ src/views/user/EditAddressProfession.js
@@ -5,9 +5,14 @@ import {
     CRow,
     CSpinner
 } from "@coreui/react";
+import { useNotification } from 'context/hooks';
 import UserEditNavComponent from 'components/userComponent/UserEditNavComponent';
 import UserEditAddressProfessionComponent from 'components/userComponent/UserEditAddressProfessionComponent';
 import ImgFondo from "../../assets/img/bg-1.png";
+import { useFormik } from "formik";
+import { updateCustomer, getCustomer } from 'services/customer';
+import { getRegion, getComuneById } from 'services/location';
+import { getEducationOptions, getProfession, getCountries } from 'services/lists';
 
 var styles = {
     backgroundImage: `url(${ImgFondo})`,
@@ -15,18 +20,115 @@ var styles = {
 };
 
 const EditAddressProfession = () => {
-    const [visible, setVisible] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [countires, setCountries] = React.useState([]);
+    const [professions, setProfessions] = React.useState([]);
+    const [regions, setRegions] = React.useState([]);
+    const [communes, setCommunes] = React.useState([]);
+    const [educations, setEducations] = React.useState([]);
+    const [, setNotification] = useNotification();
+    const formik = useFormik({
+        initialValues: {
+            day: '',
+            month: '',
+            year: '',
+            country: '',
+            region: '',
+            commune: '',
+            education_level: '',
+            occupation: '',
+            other_occupation: ''
+        },
+        onSubmit: handleUpdate
+    });
+
+    const handleUpdate = async (data) => {
+        try {
+            const res = await updateCustomer(data);
+            if (res.status >= 400) {
+                setNotification({ type: 'error', message: 'Ha ocurrido un error, no fue posible actualizar los datos.', delay: 2000 });
+            }
+            setNotification({ type: 'success', message: 'Datos actualizados correctamente.', delay: 8000 })
+        } catch (error) {
+            console.error('[handleUpdate]', error);
+        }
+    }
+
+    const handleGetCountries = async () => {
+        try {
+            const resp = await getCountries();
+            setCountries(resp.data.result)
+        } catch (error) {
+            console.log('StepOneFormComponent ERROR: ', error);
+        }
+    }
+
+    const handleGetRegion = async () => {
+        try {
+            const resp = await getRegion();
+            setRegions(resp.data.result)
+        } catch (error) {
+            console.log('StepOneFormComponent ERROR: ', error);
+        }
+    }
+
+    const handleGetCommuneById = async (region_id) => {
+        // setLoadingCommunes((prevState) => !prevState);
+        const resp = await getComuneById(region_id);
+        try {
+            setCommunes(resp.data.result);
+            // formik.setFieldValue()
+        } catch (error) {
+            console.log('ERROR: ', error);
+        }
+        // setLoadingCommunes((prevState) => !prevState);
+    }
+
+    const handleGetEducation = async () => {
+        const resp = await getEducationOptions();
+        try {
+            setEducations(resp.data.result)
+        } catch (error) {
+            console.log('ERROR: ', error);
+        }
+    }
+
+    const handleGetProfession = async () => {
+        const resp = await getProfession();
+        try {
+            setProfessions(resp.data.result)
+        } catch (error) {
+            console.log('ERROR: ', error);
+        }
+    }
+
     const handleInit = async () => {
         try {
-            setVisible(true);
+            const { data } = await getCustomer();
+            formik.setValues({
+                day: data.result.day || '',
+                month: data.result.month || '',
+                year: data.result.year || '',
+                country: data.result.country || '',
+                region: data.result.region || '',
+                commune: data.result.commune || '',
+                education_level: data.result.education_level || '',
+                occupation: data.result.occupation || '',
+                other_occupation: data.result.other_occupation || ''
+            });
+            setIsLoading(false);
         } catch (error) {
             console.log('ERROR: ', error);
         }
     }
 
     React.useEffect(() => {
+        handleGetCountries();
+        handleGetRegion();
+        handleGetEducation();
+        handleGetProfession();
         handleInit();
-    }, [])
+    }, []);
 
     return (
         <div
@@ -40,15 +142,19 @@ const EditAddressProfession = () => {
                         </div>
                     </CCol>
 
-                    <CCol md="6">
-                        <CRow className="justify-content-center mt-5">
+                    <CCol className="p-5" md="6">
+                        <CRow className="justify-content-center h-100">
                             <CCol md={8}>
                                 <UserEditNavComponent></UserEditNavComponent>
-                            </CCol>
-                        </CRow>
-                        <CRow className="justify-content-center align-items-center h-100">
-                            <CCol md={8}>
-                                {visible ? <UserEditAddressProfessionComponent /> : <div className="text-center mt-5"><CSpinner color="light" /></div>}
+                                {isLoading ? <div className="text-center mt-5"><CSpinner color="light" /></div> :
+                                    <UserEditAddressProfessionComponent
+                                        formik={formik}
+                                        countries={countires}
+                                        regions={regions}
+                                        communes={communes}
+                                        getCommunesByRegion={handleGetCommuneById}
+                                        educations={educations}
+                                        professions={professions} />}
                             </CCol>
                         </CRow>
                     </CCol>
